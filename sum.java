fc0578c5e88fed59607c0e8d67d5b7364b90cce4
Jaya Pratap Reddy Kurri <>
2021-07-15 02:11:28 -0400

    Practice Iteration & Recursion

---
+++ sum.java
@@ -0,0 +1,40 @@
+public class sum {
+    public static void main(String[] args) {
+        //System.out.println(sumWithIteration(5)); 
+        //System.out.println(sumWithRecursion(2)); 
+        printFibonacciWithIteration(10);
+    }
+
+    private static Integer sumWithRecursion(int n) {
+        if (n == 1)
+            return 1; 
+        
+        int sum =  n + sumWithRecursion(n - 1);  
+        System.out.println(sum); 
+        return sum;
+    }
+
+    private static Integer sumWithIteration(int n) {
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    private static void printFibonacciWithIteration(int n) {
+        int[] n1 = new int[n];
+        if(n > 0 ) {
+            for(int i = 0; i <= n; i++) {
+                if(i <= 1) 
+                    n1[i] = i;
+                else 
+                    n1[i] = n1[i-1] + n1[i-2];
+                    System.out.println(n1[i]);
+            }
+        }
+    }
+
+    
+}
